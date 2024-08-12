114109ce19c54dfdb3d173c7ac255c467c4c55f0
osyrisrblx <osyrisrblx@gmail.com>
2024-08-11 20:42:41 -0400

    Rewrite to use Bun

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Elysia } from "elysia";
+import log from "./log";
+
+const BANNED_SET = new Set(["dependabot[bot]"]);
+
+const app = new Elysia();
+
+app.get("/", () => "ok");
+
+app.post("/api/webhooks/*", async ({ request, path }) => {
+	const payload = await request.json();
+
+	if (BANNED_SET.has(payload?.pull_request?.user?.login) || BANNED_SET.has(payload?.head_commit?.author?.name)) {
+		log.info("Blocked request", { payload });
+		return "";
+	}
+
+	log.info("Forwarded request", { payload });
+
+	const { mode, body } = request;
+
+	const headers = new Headers(request.headers);
+	headers.delete("host");
+
+	return fetch(new Request(`https://discord.com${path}`, { mode, headers, body }));
+});
+
+app.listen(Bun.env.PORT || 8080);
+
+log.debug(`Server is running on port ${app.server?.port}`);
---
+++ src/log.ts
@@ -0,0 +1,35 @@
+import kleur from "kleur";
+
+export interface LogAttributes {
+	[index: string]: string | number | boolean | null;
+}
+
+export interface StructuredLog extends LogAttributes {
+	// (required) The content of the log
+	msg: string;
+
+	// Severity of the log
+	level: "debug" | "info" | "warn" | "error";
+}
+
+const logger = (level: StructuredLog["level"], color: kleur.Color) => (msg: string, attributes?: LogAttributes) => {
+	if (Bun.env.NODE_ENV === "production") {
+		console.log(JSON.stringify({ msg, level, ...attributes }));
+	} else {
+		const str = `[${color(level).padEnd(5, " ")}] ${msg}`;
+		if (level === "error") {
+			// for some reason, console.error is causing the first character to be red
+			// kleur.reset seems to fix this
+			console.error(kleur.reset(str));
+		} else {
+			console.log(str);
+		}
+	}
+};
+
+export default {
+	debug: logger("debug", kleur.blue),
+	info: logger("info", kleur.white),
+	warn: logger("warn", kleur.yellow),
+	error: logger("error", kleur.red),
+};
